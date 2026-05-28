71a79ded60ebc0841d0045f3a43ad74a4bf4bdf9
Suckscorpio999 <olinabin214@gmail.com>
2026-05-28 10:37:50 +0545

    feat(model): add ActivityType enum for model

---
+++ src/main/java/fitness_tracker/model/ActivityType.java
@@ -0,0 +1,15 @@
+package fitness_tracker.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+
+}
