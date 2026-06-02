db5ea71eb51c99cea89e1e0606f0a81a381de9df
Suckscorpio999 <olinabin214@gmail.com>
2026-06-02 20:55:56 +0545

    feat(repo): add UserRepository for database system

---
+++ src/main/java/fitness_tracker/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package fitness_tracker.repository;
+
+import fitness_tracker.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+}
