869dff934cded9058201c03c145cbabee02bf9be
Suckscorpio999 <olinabin214@gmail.com>
2026-06-07 12:35:18 +0545

    feat(repo): add RecommendationRepository for accessing database predefined methods

---
+++ src/main/java/fitness_tracker/repository/RecommendationRepository.java
@@ -0,0 +1,11 @@
+package fitness_tracker.repository;
+
+import fitness_tracker.model.Recommendation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface RecommendationRepository extends JpaRepository<Recommendation , String> {
+    List<Recommendation> findByUserId(String userId);
+}
