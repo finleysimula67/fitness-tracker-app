9e7d87eba8b223027d6b761327ff75173554e6f2
Suckscorpio999 <olinabin214@gmail.com>
2026-06-07 12:27:40 +0545

    feat(dto): add ActivityResponse for data transfer

---
+++ src/main/java/fitness_tracker/dto/ActivityResponse.java
@@ -0,0 +1,32 @@
+package fitness_tracker.dto;
+
+import fitness_tracker.model.ActivityType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityResponse {
+
+    private String id;
+
+    private String user_id;
+
+    private ActivityType type;
+
+    private Map<String, Object> additionalMetrics;
+
+    private Integer duration;
+
+    private Integer caloriesBurned;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
