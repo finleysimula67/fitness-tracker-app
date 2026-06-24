a9bf618d8fb3e764408d19f2ad4e65554429bbcd
Suckscorpio999 <olinabin214@gmail.com>
2026-06-24 10:02:56 +0545

    docs: add simple comments to account signup form data

--- src/main/java/fitness_tracker/dto/RegisterRequest.java
+++ src/main/java/fitness_tracker/dto/RegisterRequest.java
@@ -7,16 +7,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * This file is a sign-up form. A brand new user fills this out
+ * to create an account in our system.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+    /** The user's chosen email (System checks to make sure it's valid and not blank) */
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email")
     private String email;
 
+    /** The password the user wants to use */
     private String password;
+    /** The user's first name */
     private String firstName;
+    /** The user's last name */
     private String lastName;
+    /** The level of access given to this account (like USER or ADMIN) */
     private UserRole role;
 }
