68bd236a280323790c71ce42b9cdd274e0dfe9f6
Suckscorpio999 <olinabin214@gmail.com>
2026-06-24 10:03:13 +0545

    docs: explain the login reply data layout

--- src/main/java/fitness_tracker/dto/LoginResponse.java
+++ src/main/java/fitness_tracker/dto/LoginResponse.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * This file is the system's reply after a successful login.
+ * It hands the user their digital key and profile information.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class LoginResponse {
+    /** The secure digital key (token) used to stay logged in */
     private String token;
+    /** The profile details of the user who just logged in */
     private UserResponse userResponse;
 }
