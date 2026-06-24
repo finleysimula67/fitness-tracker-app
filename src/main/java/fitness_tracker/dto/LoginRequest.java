bd9915a010a0079bee3a4101b11daf08f59f491a
Suckscorpio999 <olinabin214@gmail.com>
2026-06-24 10:03:05 +0545

    docs: document user login form data fields

--- src/main/java/fitness_tracker/dto/LoginRequest.java
+++ src/main/java/fitness_tracker/dto/LoginRequest.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * This file is like a login slip. The user types their email
+ * and password into it to prove who they are.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
+    /** The email address of the account */
     private String email;
+    /** The password for the account */
     private String password;
 }
